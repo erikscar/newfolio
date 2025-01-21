5574781649f79ed10ad4bcde14575e83709bbea5
erikscar <erikscarcela@gmail.com>
2025-01-21 14:28:21 -0300

    Adjust in Angular.json

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -6,7 +6,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ButtonComponent, InputComponent, RouterLink],
+  imports: [ButtonComponent, InputComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
