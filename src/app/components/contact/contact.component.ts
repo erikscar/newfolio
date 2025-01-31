cb6cac9489cc1823503119e9e1c84efbdc1e8120
erikscar <erikscarcela@gmail.com>
2025-01-31 12:56:39 -0300

    Adjust in Home Section

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,41 +1,48 @@
 import { Component, inject, OnInit } from '@angular/core';
-import emailjs from '@emailjs/browser'
+import emailjs from '@emailjs/browser';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { CommonButtonComponent } from "../common-button/common-button.component";
-import { SocketService } from '../../services/socket.service';
+import { CommonButtonComponent } from '../common-button/common-button.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, CommonButtonComponent, FormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    CommonButtonComponent,
+    FormsModule,
+  ],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
-  })
+  styleUrl: './contact.component.scss',
+})
+export class ContactComponent {
+  messages: {
+    username: string;
+    newMessage: string;
+    imageLink: string;
+    createdAt: string;
+  }[] = [];
+  username: string = '';
+  newMessage: string = '';
+  imageLink: string = '';
 
-  export class ContactComponent{
-    messages: { username: string, newMessage: string, imageLink: string, createdAt: string}[]= []
-    username: string = '';
-    newMessage: string = '';
-    imageLink: string = '';
-
-    private formBuilder = inject(FormBuilder)
-    form = this.formBuilder.group({
+  private formBuilder = inject(FormBuilder);
+  form = this.formBuilder.group({
     from_name: '',
-    from_email: '', 
-    message: ''
-  })
+    from_email: '',
+    message: '',
+  });
 
-    async send() {
-    emailjs.init('YshAnHysSU2COVmMk')
-    await emailjs.send("service_90kljdk","template_283jtfl",{
+  async send() {
+    emailjs.init('YshAnHysSU2COVmMk');
+    await emailjs.send('service_90kljdk', 'template_283jtfl', {
       from_name: this.form.value.from_name,
       from_email: this.form.value.from_email,
       message: this.form.value.message,
-      });
+    });
 
-      alert("Mensagem Enviada")
-      this.form.reset()
+    alert('Mensagem Enviada');
+    this.form.reset();
   }
-
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
